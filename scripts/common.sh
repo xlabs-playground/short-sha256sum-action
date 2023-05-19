9186626a867715d8e98436540a601e21f6715935
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-05-18 23:40:19 -0300

    hardcode github registry

--- scripts/common.sh
+++ scripts/common.sh
@@ -6,7 +6,7 @@ unpublish() {
     local VERSION=${1}
     local PKG_NAME=$(package_name)
     echo "Unpublishing ${PKG_NAME}@${VERSION}..."
-    npm unpublish ${PKG_NAME}@${VERSION} --force
+    npm unpublish ${PKG_NAME}@${VERSION} --force --registry https://npm.pkg.github.com
 }
 
 install() {
